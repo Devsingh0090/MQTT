3275c2b3e39f942b9edc6fab42a8be5d83173253
Dev Kumar Singh <devsingh0050@gmail.com>
2026-02-19 10:29:48 +0530

    Add MQTT publisher to send item data
    Implement MQTT publisher that sends item data every 10 seconds.

---
+++ main/publisher.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+
+broker = "broker.hivemq.com"
+port = 1883
+topic = "aman/print/test1"
+
+client = mqtt.Client()
+client.connect(broker, port)
+
+while True:
+    data = {
+        "name": "Aman Store",
+        "item": "Milk",
+        "price": 25
+    }
+
+    client.publish(topic, json.dumps(data))
+    print("Message Sent")
+    time.sleep(10)
