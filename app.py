139b757763701c94f078e48fb147fb566638bf53
Dev Kumar Singh <devsingh0050@gmail.com>
2026-03-02 17:30:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import threading
 
 MQTT_BROKER = "test.mosquitto.org"
 MQTT_PORT = 1883
-MQTT_TOPIC = "exults0ft123"
+MQTT_TOPIC = "myhome/sensor1"
 
 app = Flask(__name__)
 
@@ -125,6 +125,7 @@ def send_message():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
